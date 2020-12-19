7aa5a995493ead0f0aeb66131e98fae33829cc32
Leon <58762081+LeonWong0609@users.noreply.github.com>
2020-12-20 02:08:02 +0800

    Create 1199.js (UCASS, 中国社会科学院大学)

---
+++ 1199.js
@@ -0,0 +1,20 @@
+var univ_list = {
+    school: "中国社会科学院研究生院", //其实已经改名了，应该称「中国社会科学院大学」
+    id: "1199",
+    link: [ {
+        name: "社科大官网",
+        url: "https://www.ucass.edu.cn/"
+    }, {
+        name: "校园信息平台",
+        url: "http://portal.gscass.cn/"
+    }, {
+        name: "社科大图书馆（内网）",
+        url: "http://10.27.0.88/"
+    }, {
+        name: "社科大邮箱",
+        url: "http://mail.ucass.edu.cn/"
+    }, {
+        name: "社科院远程访问系统",
+        url: "https://ra.cass.cn/"
+    } ]
+};
